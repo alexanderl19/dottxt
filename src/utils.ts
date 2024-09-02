0c9e58580e397e792990358d1a31dbe13decdf4a
Alexander Liu <a@alexanderliu.com>
2024-09-01 20:54:19 -0700

    refactor: remove utils

--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-export const domainPattern = (domain: string) =>
-  new RegExp(`^https?:\/([^?]+).*$`);
-
-export const snakeToTitleCase = (string: string) => {
-  string
-    .replace(/^_*(.)/, (_, char) => char.toUpperCase())
-    .replace(/[-_]+(.)/g, (_, char) => " " + char.toUpperCase());
-};
