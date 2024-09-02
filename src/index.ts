4fc4885437bff98f10728ca813eee101800b4983
Alexander Liu <a@alexanderliu.com>
2024-09-01 21:24:43 -0700

    build: switch to cloudflare workers
    cf pages forces https

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { Hono } from "hono";
-import index from "./index.txt?raw";
+import index from "./index.txt";
 
 import { usps } from "./sites/usps";
 
--- src/sites/usps.ts
+++ src/sites/usps.ts
@@ -3,11 +3,7 @@ import type { Tracker } from "@easypost/api";
 import { titleCase } from "title-case";
 import { sentenceCase } from "change-case";
 import { format } from "date-fns";
-
-const VITE_EASYPOST_API_KEY: unknown = import.meta.env.VITE_EASYPOST_API_KEY;
-if (!VITE_EASYPOST_API_KEY || typeof VITE_EASYPOST_API_KEY !== "string") {
-  throw new Error("VITE_EASYPOST_API_KEY is not set");
-}
+import { env, getRuntimeKey } from "hono/adapter";
 
 const formatDate = (date: Parameters<typeof format>["0"]) =>
   format(date, "MMM do yyyy (MM/dd/yyyy) hh:mm:ss b (HH:mm:ss)");
@@ -79,13 +75,15 @@ const usps = new Hono().on(
   "GET",
   ["/usps/tracking/:code", "/usps/tracking/:code/full"],
   async (c) => {
+    const { EASYPOST_API_KEY } = env<{ EASYPOST_API_KEY: string }>(c);
+
     const trackerResponse = await fetch(
       "https://api.easypost.com/v2/trackers",
       {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: "Basic " + btoa(VITE_EASYPOST_API_KEY + ":"),
+          Authorization: "Basic " + btoa(EASYPOST_API_KEY + ":"),
         },
         body: JSON.stringify({
           tracking_code: c.req.param("code"),
@@ -96,11 +94,10 @@ const usps = new Hono().on(
 
     if (trackerResponse.status === 200) {
       const tracker = (await trackerResponse.json()) as Tracker;
-      if (tracker.status === "unknown") {
-        return c.text(formatTracker(tracker, c.req.path.endsWith("/full")));
-      } else {
-        return c.text("Failed to fetch shipment information.", 500);
-      }
+
+      return c.text(formatTracker(tracker, c.req.path.endsWith("/full")));
+    } else {
+      return c.text("Failed to fetch shipment information.", 500);
     }
   }
 );
